9a2125226bce7613d333adccb33edeb210d606d3
Daniel Oliveira Souza <daniel.souza@tron.com.br>
2017-11-21 11:24:16 -0200

    Menu AVL

--- src/tree/avl/Main.java
+++ src/tree/avl/Main.java
@@ -1,28 +1,53 @@
 package tree.avl;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Avl tree = new Avl();
 		
-        tree.inserir(9);
-        tree.inserir(5);
-        tree.inserir(10);
-        tree.inserir(0);
-        tree.inserir(6);
-        tree.inserir(11);
-        tree.inserir(-1);
-        tree.inserir(1);
-        tree.inserir(2);
-        
-        System.out.println("Valores Inseridos");
-        
-        tree.preOrder();
-        
-        tree.remover(10);
-        
-        System.out.println("\nDelete 10");
-        tree.preOrder();
+		int command = 0;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("---- Bem Vindo ----");
+		System.out.println("Tecle 0 para encerrar");
+		System.out.println("Tecle 1 para Inserir");
+		System.out.println("Tecle 2 para listar em Pré Ordem");
+		System.out.println("Tecle 5 para Remover");
+		
+		do {
+			System.out.print("$: ");
+			command = in.nextInt();
+
+			switch (command) {
+				case 0: break;
+				case 1:	inserirArvoreAvl(in, tree); System.out.println("\n"); break;
+				case 2: System.out.println("\nPré Ordem"); tree.preOrder();; ; break;
+				case 5: removerArvoreAvl(in, tree); break;
+				default: System.out.println("Comando não especificado \n"); break;
+			}
+
+		}while(command != 0);
 	}
 
+	private static void removerArvoreAvl(Scanner in, Avl tree) {
+		System.out.print("\nValor: ");
+
+		tree.remover(in.nextInt());
+		
+		System.out.print("\nRemover outro valor? (s/n): ");
+		if(in.next().toUpperCase().equals("S"))
+			removerArvoreAvl(in, tree);
+	}
+
+	private static void inserirArvoreAvl(Scanner in, Avl tree) {
+		System.out.print("\nValor: ");
+
+		tree.inserir(in.nextInt());
+		
+		System.out.print("\nInserir outro valor? (s/n): ");
+		if(in.next().toUpperCase().equals("S"))
+			inserirArvoreAvl(in, tree);
+	}
 }
